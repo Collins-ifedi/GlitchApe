87c82f6a8ae24b43ccb1dde09fa2803fbcb5d820
Collins-ifedi <46401973+Collins-ifedi@users.noreply.github.com>
2025-10-04 05:57:20 +0100

    19999

---
+++ GlitchApe/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("404/", views.error_404, name="error_404"),
+    path("500/", views.error_500, name="error_500"),
+]
