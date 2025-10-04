098f1eccf821d0adf8cfac077054f93c0183c88f
Collins-ifedi <46401973+Collins-ifedi@users.noreply.github.com>
2025-10-04 14:15:51 +0100

    Update urls.py

--- GlitchApe/config/urls.py
+++ GlitchApe/config/urls.py
@@ -4,7 +4,7 @@ from payments.views import stripe_webhook  # will be created
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/api/", include("api.urls")),
+    path("api/", include("api.urls")),
     path("stripe/webhook/", stripe_webhook, name="stripe-webhook"),
     path("", include("core.urls")),
 ]
