b1fa59b5bb882935078f0801af1e929b85ccb9db
Collins-ifedi <46401973+Collins-ifedi@users.noreply.github.com>
2025-10-04 05:57:14 +0100

    19999

---
+++ GlitchApe/config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from payments.views import stripe_webhook  # will be created
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("/api/", include("api.urls")),
+    path("stripe/webhook/", stripe_webhook, name="stripe-webhook"),
+    path("", include("core.urls")),
+]
